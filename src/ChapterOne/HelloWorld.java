9df11ab2d83fcc83269fd2d19b5275269e8cb8ec
brianhaucke <brian.haucke@gmail.com>
2018-04-17 15:24:00 -0700

    Add files via upload

---
+++ src/ChapterOne/HelloWorld.java
@@ -0,0 +1,11 @@
+package ChapterOne;
+
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("I Rule!");
+        System.out.println("The World!");
+
+    }
+
+}
+
---
+++ src/ChapterTwo/DVDPlayer.java
@@ -0,0 +1,12 @@
+package ChapterTwo;
+
+public class DVDPlayer {
+    boolean canRecord = false;
+
+    void playDVD() {
+        System.out.println("DVD Playing");
+    }
+    void recordDVD() {
+        System.out.println("DVD recording");
+    }
+}// end DVDPlayer class

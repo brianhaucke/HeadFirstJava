f79af36ef306b1f98eadf9ce052410de74eaebb9
brianhaucke <brian.haucke@gmail.com>
2018-04-17 15:21:19 -0700

    Add files via upload

---
+++ ChapterTwo/DVDPlayerTestDrive.java
@@ -0,0 +1,14 @@
+package ChapterTwo;
+
+public class DVDPlayerTestDrive {
+    public static void main(String[] args) {
+
+        DVDPlayer d = new DVDPlayer();
+        d.canRecord = true;
+        d.playDVD();
+
+        if (d.canRecord == true) {
+            d.recordDVD();
+        }
+    }
+}// End class

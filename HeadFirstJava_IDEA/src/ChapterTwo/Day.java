b43beea36f2233f3b71c6a4c48f63e9d581bc3c6
brianhaucke <brian.haucke@gmail.com>
2018-04-23 14:23:26 -0700

    Add files via upload

--- HeadFirstJava_IDEA/src/ChapterTwo/Day.java
+++ HeadFirstJava_IDEA/src/ChapterTwo/Day.java
@@ -4,6 +4,7 @@ public class Day{
     int dayNum;
     String dayName;
     String suffix;
+    String[] dayArray = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
 
     void determineSuffix(){
@@ -18,7 +19,38 @@ public class Day{
         }
     }
 
-    void printDay(){
-        System.out.println("Today is " + dayName + ". The " + dayNum + "" + suffix + " day of the week.");
+    void determineDay(){
+        if (dayNum == 1){
+            dayName = dayArray[0];
+            determineSuffix();
+        } else if (dayNum == 2) {
+            dayName = dayArray[1];
+            determineSuffix();
+        } else if (dayNum == 3) {
+            dayName = dayArray[2];
+            determineSuffix();
+        } else if (dayNum == 4) {
+            dayName = dayArray[3];
+            determineSuffix();
+        } else if (dayNum == 5) {
+            dayName = dayArray[4];
+            determineSuffix();
+        } else if (dayNum == 6) {
+            dayName = dayArray[5];
+            determineSuffix();
+        } else if (dayNum == 7){
+            dayName = dayArray[6];
+            determineSuffix();
+        }
     }
-}
+
+    void printDay(){
+            if (dayNum >= 1 && dayNum <=7) {
+                determineDay();
+                System.out.println("Today is " + dayName + ". The " + dayNum + "" + suffix + " day of the week.");
+            } else {
+                System.out.println("Error: " + dayNum + " is an invalid day number.");
+            }
+        }
+    }
+
